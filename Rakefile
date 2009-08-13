4a26da4cb9a3564a4d0574a1019838a501c9daae
Yong Bakos <projects@humanoriented.com>
2009-08-13 17:53:37 -0600

    Removing vlad

--- Rakefile
+++
@@ -1,6 +0,0 @@
-begin
-  require "vlad"
-  Vlad.load(:app => nil, :scm => "git")
-rescue LoadError
-  # do nothing
-end
--- config/deploy.rb
+++
@@ -1,4 +0,0 @@
-set :application, "humanoriented.com"
-set :repository, "ssh://your.git.server/path/to/project/hello.git"
-set :domain, "humanoriented.com"
-set :deploy_to, "/home/humorsys/"
