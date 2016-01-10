29842c587a59c8703dcdfa79bde8628d727ca9d7
Yong Bakos <ybakos@humanoriented.com>
2016-01-10 09:46:00 -0600

    Configure deployment.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'middleman', '>= 4.0.0'
-gem 'middleman-deploy', '~> 1.0'
+# The packaged gem is behind 2.0.0, which is necessary for middleman 4.x, so get it from the source:
+gem 'middleman-deploy', :git => 'git@github.com:middleman-contrib/middleman-deploy.git'
 gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
 gem 'compass-blueprint'
--- config.rb
+++ config.rb
@@ -1,61 +1,33 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+set :haml, { :attr_wrapper => '"' }
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-set :haml, { :attr_wrapper => '"' }
-
-case ENV['TARGET'].to_s.downcase
-when 'production'
-  activate :deploy do |deploy|
-    deploy.deploy_method   = :rsync
-    deploy.host            = 'www.example.com'
-    deploy.path            = '/srv/www/production-site'
-  end
-else
-  activate :deploy do |deploy|
-    deploy.deploy_method   = :rsync
-    deploy.host            = 'staging.example.com'
-    deploy.path            = '/srv/www/staging-site'
-  end
-end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
 configure :build do
   # Minify CSS on build
   # activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
+end
+
+# Deployment config (middleman-deploy)
+case ENV['TARGET'].to_s.downcase
+when 'production'
+  activate :deploy do |deploy|
+    deploy.deploy_method   = :rsync
+    deploy.host            = 'humorsys@humanoriented.com'
+    deploy.path            = 'humanoriented.com'
+  end
+else
+  activate :deploy do |deploy|
+    deploy.deploy_method   = :rsync
+    deploy.host            = 'humorsys@staging.humanoriented.com'
+    deploy.path            = 'staging.humanoriented.com'
+  end
 end
